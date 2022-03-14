6106ea8094095b3a1c821a7ab0a65e9bd83daaf6
yumeyuniko <daigocafe@msn.com>
2022-03-14 17:29:39 +0900

    コメント投稿まで

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    posted_date = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    posted_date = models.DateTimeField(auto_now_add=True)
